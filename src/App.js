aa1bb8a0c9096b4a746afca16387c22b7d2404ed
Will Warren <willwarrenmail@gmail.com>
2019-02-21 14:04:40 -0500

    added sliders, began changing states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+
+  state = {
+    backgroundHue: ''
+    backgroundSaturation: ''
+    backgroundLightness: ''
+  }
+
+updateHue = event => {
+  this.setState({
+    backgroundHue: event.target.value
+  })
+}
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <main>
+          <section>
+            <input type="range" onChange={this.updateHue} name="Hue" min="0" max="255" />
+            <label for="Hue">Hue</label>
+            <input type="range" name="Saturation" min="0" max="255" />
+            <label for="Saturation">Saturation</label>
+            <input type="range" name="Lightness" min="0" max="255" />
+            <label for="Lightness">Lightness</label>
+          </section>
+        </main>
+      </>
+    )
   }
 }
 
