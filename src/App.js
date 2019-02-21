1c5ee8c1407cb146d0286ba86e8adb3ea402a353
Will Warren <willwarrenmail@gmail.com>
2019-02-21 15:59:06 -0500

    got sliders working, text contrast as well

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
   state = {
-    backgroundHue: ''
-    backgroundSaturation: ''
-    backgroundLightness: ''
+    backgroundHue: '196',
+    backgroundSaturation: '12',
+    backgroundLightness: '78',
+    colorOfText: 'black'
   }
 
-updateHue = event => {
-  this.setState({
-    backgroundHue: event.target.value
-  })
-}
+  updateHue = event => {
+    this.setState({
+      backgroundHue: event.target.value
+    })
+  }
+  updateSaturation = event => {
+    this.setState({
+      backgroundSaturation: event.target.value
+    })
+  }
+  updateLightness = event => {
+    this.setState({
+      backgroundLightness: event.target.value
+    })
+    if (this.state.backgroundLightness <= 40) {
+      this.setState({
+        colorOfText: 'whitesmoke'
+      })
+    }
+    if (this.state.backgroundLightness > 40) {
+      this.setState({
+        colorOfText: 'black'
+      })
+    }
+  }
+
   render() {
     return (
       <>
-        <main>
+        <main
+          style={{
+            backgroundColor: `hsl(${this.state.backgroundHue}, ${
+              this.state.backgroundSaturation
+            }%, ${this.state.backgroundLightness}%)`,
+            border: '1px solid green'
+          }}>
           <section>
-            <input type="range" onChange={this.updateHue} name="Hue" min="0" max="255" />
-            <label for="Hue">Hue</label>
-            <input type="range" name="Saturation" min="0" max="255" />
-            <label for="Saturation">Saturation</label>
-            <input type="range" name="Lightness" min="0" max="255" />
-            <label for="Lightness">Lightness</label>
+            <label style={{ color: `${this.state.colorOfText}` }}>Hue</label>
+            <input
+              type="range"
+              onChange={this.updateHue}
+              name="Hue"
+              min="0"
+              max="360"
+            />
+            <label style={{ color: `${this.state.colorOfText}` }}>
+              Saturation
+            </label>
+            <input
+              type="range"
+              onChange={this.updateSaturation}
+              name="Saturation"
+              min="0"
+              max="100"
+            />
+            <label style={{ color: `${this.state.colorOfText}` }}>
+              Lightness
+            </label>
+            <input
+              type="range"
+              onChange={this.updateLightness}
+              name="Lightness"
+              min="0"
+              max="100"
+            />
           </section>
         </main>
       </>
